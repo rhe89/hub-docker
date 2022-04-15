cdeaab0596352c3bc4b846624bddedb824951eeb
Roar <roar.h.eriksen@gmail.com>
2022-04-16 00:35:53 +0200

    New compose-files for banking and crypto-domains

--- hub/docker_deploy.sh
+++ hub/docker_deploy.sh
@@ -3,16 +3,13 @@ single_domain=$1
 # Create database
 docker-compose -f docker-compose-db.yml up -d
 
-docker pull rhe89/hub-coinbase-pro-service-bus-queue-host:latest
-docker pull rhe89/hub-coinbase-pro-scheduled-host:latest
-docker pull rhe89/hub-coinbase-pro-api:latest
-docker pull rhe89/hub-coinbase-service-bus-queue-host:latest
-docker pull rhe89/hub-coinbase-scheduled-host:latest
-docker pull rhe89/hub-coinbase-api:latest
-docker pull rhe89/hub-sbanken-service-bus-queue-host:latest
-docker pull rhe89/hub-sbanken-scheduled-host:latest
-docker pull rhe89/hub-sbanken-api:latest
-docker pull rhe89/hub-sbanken-app:latest
+docker pull rhe89/hub-crypto-service-bus-queue-host:latest
+docker pull rhe89/hub-crypto-scheduled-host:latest
+docker pull rhe89/hub-crypto-api:latest
+docker pull rhe89/hub-banking-service-bus-queue-host:latest
+docker pull rhe89/hub-banking-scheduled-host:latest
+docker pull rhe89/hub-banking-api:latest
+docker pull rhe89/hub-banking-app:latest
 docker pull rhe89/hub-spreadsheet-service-bus-queue-host:latest
 docker pull rhe89/hub-spreadsheet-api:latest
 docker pull rhe89/hub-spreadsheet-webapp:latest
@@ -20,10 +17,9 @@ docker pull rhe89/hub-spreadsheet-webapp:latest
 # If no domain is specified, deploy all
 if [ -z "$single_domain" ];
 then
-    docker-compose -f docker-compose.yml -f "docker-compose-sbanken.yml" up -d
-    docker-compose -f docker-compose.yml -f "docker-compose-coinbase.yml" up -d
+    docker-compose -f docker-compose.yml -f "docker-compose-banking.yml" up -d
     docker-compose -f docker-compose.yml -f "docker-compose-spreadsheet.yml" up -d
-    docker-compose -f docker-compose.yml -f "docker-compose-coinbase-pro.yml" up -d
+    docker-compose -f docker-compose.yml -f "docker-compose-crypto.yml" up -d
 else 
     # Deploy single domain
     docker-compose -f docker-compose.yml -f "docker-compose-$single_domain.yml" up -d
