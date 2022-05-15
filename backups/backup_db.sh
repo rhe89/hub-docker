2ea84d4d6cc5416e6dc4d2acd6f32607ac159d6b
Roar <roar.h.eriksen@gmail.com>
2022-05-15 12:09:45 +0200

    Restart policy = on-failure

--- backups/backup_db.sh
+++ backups/backup_db.sh
@@ -3,14 +3,14 @@ password=$1;
 backup_dir=$(date +'%d-%m-%Y_%H-%M-%S')
 mkdir "$backup_dir"
 
-databases=("coinbase_db" "coinbase_pro_db" "sbanken_db" "spreadsheet_db" "dashboard_db")
+databases=("coinbase_db" "binance_db" "coinbase_pro_db" "sbanken_db" "spreadsheet_db" "dashboard_db")
 
-# sudo docker exec hub_db mkdir "/var/opt/mssql/backup/$backup_dir/"
+sudo docker exec hub_db mkdir "/var/opt/mssql/backup/"$backup_dir"/"
 
 for database in ${databases[@]}; do
     sudo docker exec -it hub_db /opt/mssql-tools/bin/sqlcmd \
     -S localhost -U SA -P $password \
-    -Q "BACKUP DATABASE [$database] TO DISK = N'/var/opt/mssql/backup/$backup_dir/$database.bak' WITH NOFORMAT, NOINIT, NAME = '$database-full', SKIP, NOREWIND, NOUNLOAD, STATS = 10"
+    -Q "BACKUP DATABASE [$database] TO DISK = N'/var/opt/mssql/backup/"$backup_dir"/$database.bak' WITH NOFORMAT, NOINIT, NAME = '$database-full', SKIP, NOREWIND, NOUNLOAD, STATS = 10"
 
     sudo docker cp hub_db:/var/opt/mssql/backup/"$backup_dir"/$database.bak "$backup_dir"/$database.bak
 done
